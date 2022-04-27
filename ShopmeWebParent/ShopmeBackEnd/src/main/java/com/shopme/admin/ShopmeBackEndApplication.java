ed200eb8d406cff6fa7738b0b108139d767a4b21
dr0psh0t <xxrequiemofsoulsxx@gmail.com>
2022-04-27 16:50:53 +0800

    minor edits

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShopmeBackEndApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShopmeBackEndApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShopmeBackEndApplication.class, args);
+    }
 }
