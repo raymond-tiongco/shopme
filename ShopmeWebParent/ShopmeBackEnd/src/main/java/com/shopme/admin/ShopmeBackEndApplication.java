af3432dabc678b31032aa6dd06be3b629a4c815f
dr0psh0t <xxrequiemofsoulsxx@gmail.com>
2022-04-29 09:18:32 +0800

    minor edits

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
@@ -1,12 +1,27 @@
 package com.shopme.admin;
 
+import com.shopme.admin.service.RoleService;
+import com.shopme.admin.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ShopmeBackEndApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShopmeBackEndApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public Initializer initializer(UserService userService, RoleService roleService) {
+        return new Initializer(userService, roleService);
     }
 }
