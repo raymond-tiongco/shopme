7ef80b2a81e29118c7150001f97cc630acc00f30
dr0psh0t <xxrequiemofsoulsxx@gmail.com>
2022-04-29 09:15:42 +0800

    Role Entity

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/entity/Role.java
@@ -0,0 +1,55 @@
+package com.shopme.admin.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public Role() {}
+
+    public Role(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "["+name+","+description+"]";
+    }
+}
