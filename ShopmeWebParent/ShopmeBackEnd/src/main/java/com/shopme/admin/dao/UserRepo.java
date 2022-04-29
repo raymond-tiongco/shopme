561a80736e3e6fae9a196b1f438f0ce31f38160f
dr0psh0t <xxrequiemofsoulsxx@gmail.com>
2022-04-29 09:15:10 +0800

    added UserRepo

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/dao/UserRepo.java
@@ -0,0 +1,10 @@
+package com.shopme.admin.dao;
+
+import com.shopme.admin.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
+}
