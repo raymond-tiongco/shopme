c2f6b6459063df9de706647d9ac9a598d1403bdf
dr0psh0t <xxrequiemofsoulsxx@gmail.com>
2022-05-10 16:49:35 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# shopme daryll branch test commit 2
+# Shopme
+clone this project
+
+# Setting up project
+
+Uncomment run method from the Initializer class. This is meant to run first-time only. 
+
+![Initializer](https://raw.githubusercontent.com/raymond-tiongco/shopme/tree/daryll-shopme/ShopmeWebParent/ShopmeBackEnd/initializer.png)
+
+After running first time, comment the method body of run.
+
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/Initializer.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/Initializer.java
@@ -19,6 +19,7 @@ public class Initializer implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        //  this body is meant to run first time only
         /*
         userService.saveRole(Roles.Admin.name(), "Manage everything");
         userService.saveRole(Roles.Salesperson.name(), "Manage product price, customers, shipping, orders and sales report");
@@ -27,15 +28,15 @@ public class Initializer implements CommandLineRunner {
         userService.saveRole(Roles.Assistant.name(), "Manage product price, customers, shipping, orders and sales report");
 
         User root = new User()
-                .email("darylldagondon@gmail.com")
+                .email("superuser@gmail.com")
                 .enabled(1)
-                .firstName("Daryll David")
-                .lastName("Dagondon")
-                .password("daryll123");
+                .firstName("RootUser")
+                .lastName("RootUser")
+                .password("rootuser4567");
 
         userService.saveRootUser(root);
 
-        userService.addRoleToUser("darylldagondon@gmail.com", Roles.Admin.name());*/
-
+        userService.addRoleToUser("superuser@gmail.com", Roles.Admin.name());
+         */
     }
 }
