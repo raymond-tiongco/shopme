08add5bbdbd1900f6134029a3553939813199742
Jannielyn Acha <jannielyn.acha@fullspeedtechnologies.com>
2022-05-23 05:56:05 +0800

    Added exporter

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/exporter/UserExcelExporter.java
@@ -0,0 +1,118 @@
+package com.shopme.admin.exporter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.shopme.admin.entity.Role;
+import com.shopme.admin.entity.User;
+
+public class UserExcelExporter {
+	private XSSFWorkbook workbook;
+	private XSSFSheet sheet;
+	
+	private List<User> users;
+	
+	public UserExcelExporter(List<User> users) {
+		this.users = users;
+		
+		workbook = new XSSFWorkbook();
+		sheet = workbook.createSheet("Users");
+	}
+
+	private void writeHeaderRow() {
+		Row row = sheet.createRow(0);
+		
+		CellStyle style = workbook.createCellStyle();
+		XSSFFont font = workbook.createFont();
+		font.setBold(true);
+		font.setFontHeight(14);
+		style.setFont(font);
+		style.setFillBackgroundColor(IndexedColors.LIGHT_BLUE.getIndex());;
+		
+		Cell cell = row.createCell(0);
+		cell.setCellValue("User ID");
+		cell.setCellStyle(style);
+		
+		cell = row.createCell(1);
+		cell.setCellValue("Email");
+		cell.setCellStyle(style);
+		
+		cell = row.createCell(2);
+		cell.setCellValue("First Name");
+		cell.setCellStyle(style);
+		
+		cell = row.createCell(3);
+		cell.setCellValue("Last Name");
+		cell.setCellStyle(style);
+		
+		cell = row.createCell(4);
+		cell.setCellValue("Roles");
+		cell.setCellStyle(style);
+		
+		cell = row.createCell(5);
+		cell.setCellValue("Enabled");
+		cell.setCellStyle(style);
+	}
+	
+	private void writeDataRows() {
+		int rowCount = 1;
+		
+		for(User user: users) {
+			Row row = sheet.createRow(rowCount++);
+			
+			Cell cell = row.createCell(0);
+			cell.setCellValue(user.getId());
+			sheet.autoSizeColumn(0);
+			
+			cell = row.createCell(1);
+			cell.setCellValue(user.getEmail());
+			sheet.autoSizeColumn(1);
+			
+			cell = row.createCell(2);
+			cell.setCellValue(user.getFirstName());
+			sheet.autoSizeColumn(2);
+			
+			cell = row.createCell(3);
+			cell.setCellValue(user.getLastName());
+			sheet.autoSizeColumn(3);
+			
+			cell = row.createCell(4);
+			List<String> roles = new ArrayList<>();
+			for(Role role: user.getRoles()) {
+				roles.add(role.getName());
+			}
+			String rolesString = String.join(", ", roles);
+			cell.setCellValue(rolesString);
+			sheet.autoSizeColumn(4);
+			
+			cell = row.createCell(5);
+			cell.setCellValue(user.getEnabled());
+			sheet.autoSizeColumn(5);
+		}
+		
+		
+	}
+	
+	public void export(HttpServletResponse response) throws IOException {
+		writeHeaderRow();
+		writeDataRows();
+		
+		ServletOutputStream outputStream = response.getOutputStream();
+		workbook.write(outputStream);
+		workbook.close();
+		outputStream.close();
+	}
+
+}
---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/exporter/UserPDFExporter.java
@@ -0,0 +1,96 @@
+package com.shopme.admin.exporter;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import com.shopme.admin.entity.Role;
+import com.shopme.admin.entity.User;
+
+public class UserPDFExporter {
+	private List<User> users;
+
+	public UserPDFExporter(List<User> users) {
+		this.users = users;
+	}
+	
+	private void writeTableHeader(PdfPTable table) {
+		PdfPCell cell = new PdfPCell();
+		cell.setBackgroundColor(Color.DARK_GRAY);
+		cell.setPadding(5);
+		
+		Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+		font.setColor(Color.WHITE);
+		font.setSize(12);
+		
+		cell.setPhrase(new Phrase("UserID", font));
+		table.addCell(cell);
+		
+		cell.setPhrase(new Phrase("Email", font));
+		table.addCell(cell);
+		
+		cell.setPhrase(new Phrase("First Name", font));
+		table.addCell(cell);
+		
+		cell.setPhrase(new Phrase("Last Name", font));
+		table.addCell(cell);
+		
+		cell.setPhrase(new Phrase("Roles", font));
+		table.addCell(cell);
+		
+		cell.setPhrase(new Phrase("Enabled", font));
+		table.addCell(cell);
+	}
+	
+	private void writeTableData(PdfPTable table) {
+		for(User user: users) {
+			table.addCell(String.valueOf(user.getId()));
+			table.addCell(user.getEmail());
+			table.addCell(user.getFirstName());
+			table.addCell(user.getLastName());
+			List<String> roles = new ArrayList<>();
+			for(Role role: user.getRoles()) {
+				roles.add(role.getName());
+			}
+			String rolesString = String.join(", ", roles);
+			table.addCell("[" + rolesString + "]");
+			table.addCell(String.valueOf(user.getEnabled()));
+		}
+	}
+	
+	public void export(HttpServletResponse response) throws DocumentException, IOException {
+		Document document = new Document(PageSize.A4);
+		
+		PdfWriter.getInstance(document, response.getOutputStream());
+		
+		document.open();
+		
+		Paragraph title = new Paragraph("List of All Users");
+		document.add(title);
+		
+		PdfPTable table = new PdfPTable(6);
+		table.setWidthPercentage(100);
+		table.setSpacingBefore(15);
+		
+		writeTableHeader(table);
+		writeTableData(table);
+		
+		document.add(table);
+		
+		document.close();
+		
+	}
+}
