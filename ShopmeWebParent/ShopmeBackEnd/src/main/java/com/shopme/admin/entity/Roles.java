e83b504ddd21d630bbce8ffdc2700abb67f78ce6
dr0psh0t <xxrequiemofsoulsxx@gmail.com>
2022-05-25 11:55:44 +0800

    may 25 push

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/entity/Roles.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/entity/Roles.java
@@ -1,17 +1,15 @@
 package com.shopme.admin.entity;
 
 public enum Roles {
-    Admin(1,"Manage everything"),
-    Salesperson(2,"Manage product price, customers, shipping, orders and sales report"),
-    Editor(3,"Manage categories, brands, products, articles and menus"),
-    Shipper(4,"View products, view orders and update order status"),
-    Assistant(5,"Manage product price, customers, shipping, orders and sales report");
+    Admin("Manage everything"),
+    Salesperson("Manage product price, customers, shipping, orders and sales report"),
+    Editor("Manage categories, brands, products, articles and menus"),
+    Shipper("View products, view orders and update order status"),
+    Assistant("Manage product price, customers, shipping, orders and sales report");
 
-    public final int ROLE_NUMBER;
     public final String DESCRIPTION;
 
-    Roles(int roleNumber, String description) {
-        this.ROLE_NUMBER = roleNumber;
+    Roles(String description) {
         this.DESCRIPTION = description;
     }
 }
