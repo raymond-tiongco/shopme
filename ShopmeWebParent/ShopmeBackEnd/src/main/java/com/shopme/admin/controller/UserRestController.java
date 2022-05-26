6a934a5244f8318c9b8e72b347bccb0d199d3f9f
Jannielyn Acha <jannielyn.acha@fullspeedtechnologies.com>
2022-05-27 00:06:52 +0800

    added UserRestController

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/UserRestController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/UserRestController.java
@@ -1,43 +1,30 @@
 package com.shopme.admin.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.shopme.admin.dao.UserRepository;
-import com.shopme.admin.entity.User;
+import com.shopme.admin.service.UserService;
 
 @RestController
 @RequestMapping("/rest/users")
 public class UserRestController {
-	private final UserRepository userRepository;
+	private final UserService userService;
 
 	@Autowired
-	public UserRestController(UserRepository userRepository) {
-		this.userRepository = userRepository;
+	public UserRestController(UserService userService) {
+		this.userService = userService;
 	}
 	
-	@GetMapping
-	List<User> all() {
-		return userRepository.findAll();
+	@PostMapping("/checkEmail")
+	public String checkDuplicateEmail(@Param("id") Integer id, @Param("email") String email) {
+		if(!userService.isUniqueEmail(id, email)) {
+			return "Email is duplicated.";
+		}
+		else {
+			return "Ok";
+		}
 	}
-	
-	@GetMapping("/search/{keyword}")
-    private List<User> searchUsers(@PathVariable String keyword, Model theModel) {
-
-        System.out.println("[UserController] Searching...");
-        List<User> users = userRepository.findByFirstNameOrLastNameOrEmail(keyword);
-        
-        if(users.isEmpty()) System.out.println("[UserController] Sorry, user is not found!");
-        else {
-        	System.out.println("Users found! users: " + users);
-        }
-        
-        return users;
-    }
 }
