27dd15159f5b8a92f55e8ae40de683a2f80560fa
Jannielyn Acha <jannielyn.acha@fullspeedtechnologies.com>
2022-06-16 10:33:05 +0800

    Modified login controller for login input validation

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/LoginController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/LoginController.java
@@ -1,6 +1,13 @@
 package com.shopme.admin.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,8 +18,26 @@ public class LoginController {
         return "user_login";
     }
 	
-	@GetMapping("/access-denied")
+	@GetMapping("/accessDenied")
 	public String accessDenied() {
-	    return "access-denied";
+	    return "access_denied";
 	}
+	
+	@GetMapping("/loginError")
+    public String login(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession(false);
+        String errorMessage = null;
+        if (session != null) {
+            AuthenticationException ex = (AuthenticationException) session
+                    .getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+            if(ex instanceof BadCredentialsException) {
+            	errorMessage = "Invalid email and password";
+            }
+            else if (ex != null) {
+            	errorMessage = ex.getMessage();
+            }
+        }
+        model.addAttribute("errorMessage", errorMessage);
+        return "user_login";
+    }
 }
